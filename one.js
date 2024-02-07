0ce795540fc7bfce67c250881b7426c63bae1ae8
ashishpradhan7207 <ashishpradhan7207@gmail.com>
2024-02-07 19:03:13 +0530

    tst

--- one.js
+++ one.js
@@ -1,1 +1,10 @@
-console.log(10+20);
+let emp={
+    id:110,
+    name:"Rahul"
+}
+let employees = [{},{},null]
+console.log(emp.id)   //110
+console.log(emp.name) //Rahul
+console.log(emp.length) //undefined
+console.log(emp.nayana) //undefined
+console.log(employees.length) //3
